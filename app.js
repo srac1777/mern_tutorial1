3a1678c5cfdcfb706724c86a6dd1627d7ab9ce0f
srac1777 <shashank.racherla@gmail.com>
2018-08-24 14:31:34 -0700

    push

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const db = require("./config/keys").mongoURI;
+const users = require('./routes/api/users');
+const events = require('./routes/api/events');
 
 mongoose
     .connect(db)
@@ -9,6 +12,11 @@ mongoose
     .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('hello cory heyy'));
+app.use('/api/users', users);
+app.use('/api/events', events);
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
